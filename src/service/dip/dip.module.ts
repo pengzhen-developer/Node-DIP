585a2aa21bb9917cec25488302fced863933bdd0
PengZhen <peng_zhen@outlook.com>
2023-04-11 14:53:15 +0800

    feat: 新增荆门市分组器

--- src/service/dip/dip.module.ts
+++ src/service/dip/dip.module.ts
@@ -8,6 +8,7 @@ import { RegionStrategyService } from './utils/region.strategy.service'
 import { RegionBaseService } from './utils/region.base.service'
 import { Region_420500_2021 } from './utils/region.420500.2021'
 import { Region_420500_2022 } from './utils/region.420500.2022'
+import { Region_420800_2023 } from './utils/region.420800.2023'
 import { Region_420900_2022 } from './utils/region.420900.2022'
 import { Region_421000_2021 } from './utils/region.421000.2021'
 import { Region_421000_2022 } from './utils/region.421000.2022'
@@ -15,6 +16,16 @@ import { Region_421000_2022 } from './utils/region.421000.2022'
 @Module({
   imports: [TypeOrmModule.forFeature([DipTodo, DipTodoResult])],
   controllers: [DipController],
-  providers: [DipService, RegionStrategyService, RegionBaseService, Region_420500_2021, Region_420500_2022, Region_420900_2022, Region_421000_2021, Region_421000_2022]
+  providers: [
+    DipService,
+    RegionStrategyService,
+    RegionBaseService,
+    Region_420500_2021,
+    Region_420500_2022,
+    Region_420800_2023,
+    Region_420900_2022,
+    Region_421000_2021,
+    Region_421000_2022
+  ]
 })
 export class DipModule {}
