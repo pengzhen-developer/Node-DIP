96fac68f2f16b0a6c1900ca7fda253ef405521cf
PengZhen <peng_zhen@outlook.com>
2023-05-12 11:27:23 +0800

    feat: 优化日志显示格式

--- src/common/interceptor/logging.interceptor.ts
+++ src/common/interceptor/logging.interceptor.ts
@@ -1,4 +1,5 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common'
+import { Response } from 'express'
 import { Observable } from 'rxjs'
 import { tap } from 'rxjs/operators'
 
@@ -13,13 +14,15 @@ export class LoggingInterceptor implements NestInterceptor {
 
         // 打印请求方法，请求链接，处理时间和响应数据
         Logger.log(
-          `${new Date().toLocaleString('zh-Hans-CN')} ` +
-            `${Date.now() - startTime}ms ` +
-            `${request.method} ` +
-            `${request.url} ` +
-            `query:${JSON.stringify(request.query)} ` +
-            `params:${JSON.stringify(request.params)} ` +
-            `body:${JSON.stringify(request.body)}`,
+          `\n` +
+            `Request Time       :   ${new Date().toLocaleString('zh-Hans-CN')} \n` +
+            `Request Duration   :   ${Date.now() - startTime}ms \n` +
+            `Request Url        :   ${request.url} \n` +
+            `Request Method     :   ${request.method} \n` +
+            `Request Query      :   ${JSON.stringify(request.query)} \n` +
+            `Request Params     :   ${JSON.stringify(request.params)} \n` +
+            `Request Body       :   ${JSON.stringify(request.body)} \n` +
+            `Response Data      :   ${JSON.stringify(response.data)}`,
           'LoggingInterceptor'
         )
       })
--- src/common/interceptor/transform.interceptor.ts
+++ src/common/interceptor/transform.interceptor.ts
@@ -1,4 +1,4 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common'
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common'
 import { map, Observable } from 'rxjs'
 import { name, version } from './../../../package.json'
 
@@ -7,6 +7,9 @@ export class TransformInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data) => {
+        // 打印请求方法，请求链接，处理时间和响应数据
+        Logger.log(`${new Date().toLocaleString('zh-Hans-CN')} ` + `Data:${JSON.stringify(data)} `, 'TransformInterceptor')
+
         return {
           name: name,
           version: version,
